c968dedceac8dd9cee2f626131fbf34a35d0ddad
Pa <past80@gmail.com>
2024-02-15 09:52:05 +0400

    added set -xe to exit on errors

--- ubuntu-kubectl-alias.sh
+++ ubuntu-kubectl-alias.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-
 ## Настройка короткого альяса `k` для `kubectl`
+
+## Добавил set -xe чтобы выходить в случае ошибок
+set -xe
 
 ## 1. Установка bash-completion
 sudo apt update -y && sudo apt install bash-completion -y
 
 ## 2. Установка complete-alias : https://github.com/cykerway/complete-alias
 cd && wget https://github.com/prostopasta/complete-alias/raw/master/complete_alias && mv complete_alias ~/.complete_alias
-echo ". ~/.complete_alias" >> ~/.bash_completion
+grep -wq '^. ~/.complete_alias' ~/.complete_alias ~/.bash_completion || echo ". ~/.complete_alias" >> ~/.bash_completion
 
 ## 3. Зададим completion для kubectl
 grep -wq '^source <(kubectl completion bash)' ~/.bashrc || echo 'source <(kubectl completion bash)' >> ~/.bashrc
 
 ## 4. Настроим alias k=kubectl
-alias k=kubectl
-grep -wq '^alias k=kubectl' ~/.bashrc || echo 'alias k=kubectl'>>~/.bashrc
-source ~/.bashrc
-echo "complete -F _complete_alias k" >> ~/.complete_alias
+grep -wq '^alias k=kubectl' ~/.bashrc || echo 'alias k=kubectl' >> ~/.bashrc
+grep -wq '^complete -F _complete_alias k' ~/.complete_alias || echo 'complete -F _complete_alias k' >> ~/.complete_alias
 
 ## 5. Готово
-echo "Проверьте работу команды 'k', напечатайте в консоли k <tab>"
+echo "Теперь перезапустите сессию bash или выполните в консоли: $ source ~/.bashrc .."
+echo ".. Проверьте работу команды 'k', напечатайте в консоли k <tab>"
